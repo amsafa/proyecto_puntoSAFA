bd657183dfb2e3df4cac5c0dc1b951408113734d
alba <sfg@sfg.es>
2025-01-28 18:56:02 +0100

    modificando footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import {HeaderComponent} from './component/header/header.component';
+import {FooterComponent} from './component/footer/footer.component';
 
 export const routes: Routes = [
 
   {path: 'header', component:HeaderComponent, pathMatch: 'full'},
+  {path: 'footer', component: FooterComponent, pathMatch: 'full'},
 ];
