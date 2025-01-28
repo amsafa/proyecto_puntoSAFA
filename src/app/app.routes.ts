e87bb757e7a78f1874f524074d2f2dbad97ca3b5
alba <sfg@sfg.es>
2025-01-28 23:50:33 +0100

    Hecho el HeroSectionInicio.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import {HeaderComponent} from './component/header/header.component';
 import {FooterComponent} from './component/footer/footer.component';
+import {HeroSectionInicioComponent} from './component/hero-section-inicio/hero-section-inicio.component';
 
 export const routes: Routes = [
 
   {path: 'header', component:HeaderComponent, pathMatch: 'full'},
   {path: 'footer', component: FooterComponent, pathMatch: 'full'},
+  {path: 'heroInicio', component: HeroSectionInicioComponent, pathMatch: 'full'},
 ];
