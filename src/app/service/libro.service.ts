cd24c3d088316b9255eb6295c32a6bf61acee086
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2025-02-06 20:07:16 +0100

    Modificado metodo searchBooks para autor objeto en libro

--- src/app/service/libro.service.ts
+++ src/app/service/libro.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import {Libro} from '../interface/libro';
-import {Observable} from 'rxjs';
-import {HttpClient} from '@angular/common/http';
+
 
 
 @Injectable({
@@ -10,7 +9,7 @@ import {HttpClient} from '@angular/common/http';
 export class LibroService {
   private baseUrl: string = "http://127.0.0.1:8000/libro";
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   async getLibros(): Promise<Libro[]> {
     const response = await fetch(`${this.baseUrl}/all`);
@@ -19,12 +18,6 @@ export class LibroService {
     }
     return await response.json();
   }
-
-getLibrosByPrecio(range:string):Observable<Libro[]> {
-    return this.http.get<Libro[]>(`${this.baseUrl}/precios`);
-}
-
-
 
 
 }
