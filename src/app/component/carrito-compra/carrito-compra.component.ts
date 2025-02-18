f9599d9c49a9eace4c5d642709deadab015f836d
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2025-02-18 19:06:50 +0100

    Añadido fondo azul a cantidad producto no visible

--- src/app/component/carrito-compra/carrito-compra.component.ts
+++ src/app/component/carrito-compra/carrito-compra.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {LibroCarrito} from '../../interface/libro-carrito';
 import {CarritoService} from '../../service/carrito.service';
 import {CurrencyPipe, NgForOf, NgIf} from '@angular/common';
-import {Router} from "@angular/router";
+import {Route, Router} from "@angular/router";
+import {AuthService} from '../../service/auth.service';
 
 
 @Component({
@@ -21,11 +22,19 @@ export class CarritoCompraComponent implements OnInit {
   showCart: boolean = false;
   baseTotal:number =0;
   totalWithTaxes:number = 0;
+  isLoggedIn = false;
 
-  constructor(private carritoService: CarritoService, private router: Router) { }
+  constructor(private carritoService: CarritoService, private router: Router, private authService:AuthService) { }
 
   ngOnInit(): void {
     // Subscribe to cart items to update the cart
+    this.authService.getAuthState().subscribe((state) => {
+      this.isLoggedIn = state;
+      if (this.isLoggedIn) {
+        this.authService.fetchUserData();
+      }
+    });
+
     this.carritoService.getCartItems().subscribe(items => {
       this.cartItems = items;
       const {baseTotal, totalWithTaxes} = this.carritoService.getTotalPrice();
@@ -33,6 +42,7 @@ export class CarritoCompraComponent implements OnInit {
       this.totalWithTaxes = totalWithTaxes;
 
     });
+
 
     // Subscribe to cart visibility
     this.carritoService.showCart$.subscribe(show => {
