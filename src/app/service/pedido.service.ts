a3474ed53de08d20c2dcc1842a0af57ef7b50cb9
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2025-02-21 13:16:08 +0100

    Añadido metodo obtener estadisticas cliente

--- src/app/service/pedido.service.ts
+++ src/app/service/pedido.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Pedido} from '../interface/pedido';
 import {Observable} from 'rxjs';
+import {EstadisticasPedidos} from '../interface/estadisticas-pedidos';
 
 @Injectable({
   providedIn: 'root'
@@ -14,5 +15,10 @@ export class PedidoService {
   getPedidosByCliente(clienteId: number, token: string | null): Observable<Pedido[]> {
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
     return this.http.get<Pedido[]>(`${this.apiUrl}/cliente/${clienteId}`, { headers });
+  }
+
+  getOrderStatsByClient(clientId: number, token: string | null): Observable<EstadisticasPedidos> {
+    const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
+    return this.http.get<any>(`${this.apiUrl}/cliente/${clientId}/estadisticas`, { headers });
   }
 }
