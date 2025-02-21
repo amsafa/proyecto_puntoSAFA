6b88208d44d843151f63280f8aceb62bb2bd9fa3
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2025-02-21 10:14:30 +0100

    Cambiado atributo direccion por direccion_entrega para que corresponda con API

--- src/app/interface/pedido.ts
+++ src/app/interface/pedido.ts
@@ -4,7 +4,7 @@ export interface Pedido {
   fecha: string;
   total: number;
   estado: string;
-  direccion: string;
+  direccion_entrega: string;
   cliente: number; // Client ID
   lineaPedidos: LineaPedido[];
 }
