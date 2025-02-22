375ff441e57689d8780b40bba21a32f3734e4a4c
alba <sfg@sfg.es>
2025-02-22 01:22:11 +0100

    Correciones en reseña y califiacionMedia

--- src/app/service/resena.service.ts
+++ src/app/service/resena.service.ts
@@ -1,31 +1,44 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Libro } from '../interface/libro'; // Importar la interfaz de libro
-import {Observable} from 'rxjs';
+import { Observable } from 'rxjs';
 import { Resena } from '../interface/resena';
-import {environment} from '../../environments/environment';
-
-
+import { environment } from '../../environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-
 export class ResenaService {
-  private baseUrlResena:string =  `${environment.apiUrl}/resena`;
+  private baseUrlResena: string = `${environment.apiUrl}/resena`;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
+  /**
+   * Obtener todas las reseñas de un libro.
+   */
   obtenerResenasPorLibro(idLibro: number): Observable<Resena[]> {
     return this.http.get<Resena[]>(`${this.baseUrlResena}/resenas/${idLibro}`);
   }
 
-  obtenerMediaCalificacion(idLibro: number): Observable<number> {
-    return this.http.get<number>(`${this.baseUrlResena}/media-calificacion/${idLibro}`);
-
+  /**
+   * Obtener la media de calificación de un libro.
+   */
+  // En resena.service.ts
+  obtenerMediaCalificacion(id: number): Observable<{ mediaCalificacion: number }> {
+    return this.http.get<{ mediaCalificacion: number }>(`${this.baseUrlResena}/media-calificacion/${id}`);
   }
-
+  /**
+   * Obtener los 3 libros mejor calificados.
+   */
   obtener3topLibros(): Observable<Libro[]> {
     return this.http.get<Libro[]>(`${this.baseUrlResena}/top-libros`);
+  }
+
+  /**
+   * Enviar una nueva reseña.
+   * La verificación de compra se realiza automáticamente en el backend.
+   */
+  enviarResena(resena: { libro: number | undefined; calificacion: number; comentario: string }): Observable<Resena> {
+    return this.http.post<Resena>(`${this.baseUrlResena}/nueva`, resena);
   }
 }
