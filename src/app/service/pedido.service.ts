719cf1e7d5daa1147dc8d5850d80a99ee768980c
pablorodbriguezbravo <prodriguezbravo@safareyes.es>
2025-02-28 20:46:47 +0100

    Creados etodos listar pedidos pendientes (getPendingPedidos) y actualizar estado pedido (updateEstado)

--- src/app/service/pedido.service.ts
+++ src/app/service/pedido.service.ts
@@ -3,6 +3,7 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Pedido} from '../interface/pedido';
 import {Observable} from 'rxjs';
 import {EstadisticasPedidos} from '../interface/estadisticas-pedidos';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
@@ -10,15 +11,29 @@ import {EstadisticasPedidos} from '../interface/estadisticas-pedidos';
 export class PedidoService {
 
   constructor(private http: HttpClient) { }
-  private apiUrl = 'http://127.0.0.1:8000/pedido';
+  private apiUrl = environment.apiUrl;
 
   getPedidosByCliente(clienteId: number, token: string | null): Observable<Pedido[]> {
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    return this.http.get<Pedido[]>(`${this.apiUrl}/cliente/${clienteId}`, { headers });
+    return this.http.get<Pedido[]>(`${this.apiUrl}/pedido/cliente/${clienteId}`, { headers });
   }
 
   getOrderStatsByClient(clientId: number, token: string | null): Observable<EstadisticasPedidos> {
     const headers = new HttpHeaders().set('Authorization', `Bearer ${token}`);
-    return this.http.get<any>(`${this.apiUrl}/cliente/${clientId}/estadisticas`, { headers });
+    return this.http.get<any>(`${this.apiUrl}/pedido/cliente/${clientId}/estadisticas`, { headers });
   }
+
+  getPendingPedidos(): Observable<Pedido[]> {
+    return this.http.get<Pedido[]>(`${this.apiUrl}/pedido/all/pendientes`);
+  }
+
+  updateEstado(pedidoId: number | undefined, newEstado: string): Observable<Pedido> {
+    return this.http.put<Pedido>(
+      `${this.apiUrl}/pedido/${pedidoId}/estado`,
+      { estado: newEstado },
+      { headers: { 'Content-Type': 'application/json' } } // Explicitly set JSON headers
+    );
+  }
+
+
 }
